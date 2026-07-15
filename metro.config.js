13d463a902ab6170abe11747e38dea7eef91e5b4
2EK66 <ben668869@gmail.com>
2026-07-15 09:10:36 +0000

    fix: metro config sans destructuring

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,6 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+const nativewind = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+module.exports = nativewind.withNativeWind(config, { input: "./global.css" });
