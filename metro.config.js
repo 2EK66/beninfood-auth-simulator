f1b5e12255585a8ce416ad8d17870eab2285a84b
2EK66 <ben668869@gmail.com>
2026-07-15 09:43:37 +0000

    fix: stream-browserify polyfill pour supabase realtime

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,9 @@
 const expo = require("expo/metro-config");
 const config = expo.getDefaultConfig(__dirname);
+
+config.resolver = config.resolver || {};
+config.resolver.extraNodeModules = {
+  stream: require.resolve("stream-browserify")
+};
+
 module.exports = config;
