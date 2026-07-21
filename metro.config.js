22e76c61066b9ce2cd532dfee6af67a8f90b2b76
2EK66 <ben668869@gmail.com>
2026-07-21 02:37:36 +0100

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,10 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// Désactive la minification agressive si elle fait boucler le bundler sous Linux
+config.transformer = {
+  ...config.transformer,
+  minifierPath: require.resolve("metro-minify-terser"),
+};
+
 module.exports = withNativeWind(config, { input: "./global.css" });
