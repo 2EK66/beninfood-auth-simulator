7143820de647da91d9674249e75be6cd9bb6651a
2EK66 <ben668869@gmail.com>
2026-07-22 05:49:44 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bf-dark": "#001f13",
+        "bf-card": "#002d1c",
+        "bf-border": "#003d26",
+        "bf-yellow": "#fcd116",
+        "bf-green": "#34d399",
+      },
+    },
   },
   plugins: [],
 };
